f3010719fa2fee57a5e16591ff237fa095e15540
kathiouchka <yohann.beaure@gmail.com>
2024-07-20 01:14:06 +0200

    fix: GOOD NIGHT

--- src/services/webhookServer.js
+++ src/services/webhookServer.js
@@ -79,9 +79,9 @@ app.post('/webhook', async (req, res) => {
                 buyWaitAndSell(NEW_TOKEN_ADDRESS);
             }
         } else if (event[0].type === 'TRANSFER' && event[0].nativeTransfers && event[0].nativeTransfers.length > 0 && !event[0].tokenTransfers) {
-            log(LOG_LEVELS.INFO, `D5scription: ${event[0].description}`, true, true, "So11111111111111111111111111111111111111112");
+            log(LOG_LEVELS.INFO, `${event[0].description}`, true, true, "So11111111111111111111111111111111111111112");
         } else if (event[0].type === 'TRANSFER') {
-            log(LOG_LEVELS.INFO, `D1scription: ${event[0].description}`, true, true, event[0].tokenTransfers[0].mint);
+            log(LOG_LEVELS.INFO, `${event[0].description}`, true, true, event[0].tokenTransfers[0].mint);
         } else {
             log(LOG_LEVELS.INFO, `Description: ${event[0].description}`, true, true);
         }
