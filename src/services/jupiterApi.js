7ea93c05278c3fd866f597ec6b9c2833ff39461e
kathiouchka <yohann.beaure@gmail.com>
2024-07-23 09:09:06 +0200

    fix: remove direct routing

--- src/services/jupiterApi.js
+++ src/services/jupiterApi.js
@@ -111,7 +111,7 @@ async function tradeTokenWithJupiter(tokenAddress, percentage, isBuy = true, sli
                 });
             }
 
-            const response = await fetch(`https://quote-api.jup.ag/v6/quote?inputMint=${inputMint}&outputMint=${outputMint}&amount=${amount}&onlyDirectRoutes=true&slippageBps=${slippage * 100}`);
+            const response = await fetch(`https://quote-api.jup.ag/v6/quote?inputMint=${inputMint}&outputMint=${outputMint}&amount=${amount}&slippageBps=${slippage * 100}`);
 
             if (!response.ok) {
                 throw new Error(`HTTP quote error! status: ${response.statusText}`);
