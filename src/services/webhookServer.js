16639e788025287de714dd6b1af639e0c80c2dfa
kathiouchka <yohann.beaure@gmail.com>
2024-08-08 19:31:55 +0200

    fix now buy only the half

--- src/services/webhookServer.js
+++ src/services/webhookServer.js
@@ -243,7 +243,7 @@ app.post('/webhook', async (req, res) => {
                             log(LOG_LEVELS.INFO, `Waited ${delay / 1000} seconds. Initiating buy.`, {
                                 isBot: true,
                             });
-                            await tradeTokenWithJupiter(currentTokenState.NEW_TOKEN_ADDRESS, 90, true, 10);
+                            await tradeTokenWithJupiter(currentTokenState.NEW_TOKEN_ADDRESS, 50, true, 10);
                             currentTokenState.TOKEN_BOUGHT = true;
                         }, delay);
 
